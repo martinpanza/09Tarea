5463aa5604ce075fa9e4a20ac20df40b2aa2e5f0
Martín!!! <martin_panza_p@hotmail.com>
2015-11-24 17:53:44 -0300

    Finaliza parte2

--- Parte2.py
+++ Parte2.py
@@ -18,6 +18,33 @@ def H(v, d):
     return H
 
 datos = np.loadtxt('SNIa*.dat')
-#datos=np.genfromtxt('hubble_original.dat')
+
 distancia = datos[:, 0]
 velocidad = datos[:, 1]
+
+H0 = np.linspace(0, 36, 36)
+
+np.random.seed(111)
+muestra = H(velocidad, distancia)
+
+N = len(muestra)
+Nboot = 100000
+mean_values = np.zeros(Nboot)
+
+for i in range(Nboot):
+    s = np.random.randint(low=0, high=N, size=N)
+    mean_values[i] = np.mean(muestra[s])
+
+fig2 = plt.figure(2)
+fig2.clf()
+plt.hist(mean_values, bins=30)
+plt.axvline(np.mean(muestra), color='r')
+plt.xlabel('H0 (constante de Hubble) [km / s / Mpc]')
+plt.ylabel('V/D (velocidad/distancia) [km / s / Mpc]')
+plt.legend('H','r')
+plt.show()
+
+mean_values = np.sort(mean_values)
+limite_bajo = mean_values[int(Nboot * 0.05)]
+limite_alto = mean_values[int(Nboot * 0.95)]
+print "El intervalo de confianza al 95% es: [{}:{}]".format(limite_bajo, limite_alto)
